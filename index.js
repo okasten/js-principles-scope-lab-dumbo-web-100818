837e631ad19724db2910c9463a7652e4b03c7981
Olivia Kasten <odkasten@gmail.com>
2018-11-18 15:01:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = "bob";
 
-function upperCaseCapitalName(name){
+function upperCaseCustomerName(name){
   customerName = customerName.toUpperCase();
   return customerName;
 }
